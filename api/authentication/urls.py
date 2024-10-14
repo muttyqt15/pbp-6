3d583060e95e42fcb30c3cd6384a58cb4eba51a2
muttyqt15 <hayaye69@gmail.com>
2024-10-15 03:38:44 +0700

    feat: setup media url

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import login
+from .views import login, sign_up
 
 urlpatterns = [
     path("login/", login, name="login"),
+    path("register/", sign_up, name="sign-up"),
 ]
--- api/authentication/views.py
+++ api/authentication/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import LoginForm, SignUpForm
 
 
-# Create your views here.
 def login(request):
-    form = SignUpForm()
     if request.method == "POST":
-        form = SignUpForm(request.POST)
+        form = LoginForm(request.POST)
         if form.is_valid():
-            form.save()
+            return redirect("/admin")
 
+
+def sign_up(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST, request.FILES)  # Include request.FILES
+        if form.is_valid():
+            form.save()  # This saves the user and the uploaded file
+            return redirect("/admin/")
+    else:
+        form = SignUpForm()
     return render(request, "login.html", {"form": form})
