769b91227d43d72cd092ea983b68b20ff26e0412
muttyqt15 <hayaye69@gmail.com>
2024-10-23 15:38:43 +0700

    init model

--- api/thread/models.py
+++ api/thread/models.py
@@ -7,8 +7,12 @@ class Thread(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="threads")
     image = models.ImageField(upload_to="thread/", null=True, blank=True)
+    likes = models.ManyToManyField(User, related_name="likes_thread", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def get_likes_count(self):
+        return self.likes.count()
 
     def __str__(self):
         return self.title
@@ -19,5 +23,10 @@ class Comment(models.Model):
     thread = models.ForeignKey(
         Thread, on_delete=models.CASCADE, related_name="comments"
     )
+    content = models.TextField()
+    likes = models.ManyToManyField(User, related_name="likes_comment", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def get_likes_count(self):
+        return self.likes.count()
