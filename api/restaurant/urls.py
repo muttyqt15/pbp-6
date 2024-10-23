c9e4d9222598b5d62f1bc0955f440505ac0e46cd
rigelia <f3atha04@gmail.com>
2024-10-23 15:32:39 +0700

    added edit functionality to restaurant information (except for add category)

--- api/restaurant/urls.py
+++ api/restaurant/urls.py
@@ -5,7 +5,6 @@ from .views import add_restaurant, edit_restaurant, get_restaurant_xml, get_rest
 urlpatterns: list[URLResolver] = [
         path("", my_restaurant, name="my_restaurant"),
         path("add/", add_restaurant, name="add_restaurant"),
-        path("edit/<int:id>/", edit_restaurant, name="edit_restaurant"),
         path("<int:id>/menu/", get_restaurant_menu, name="get_restaurant_menu"),
         path("get/", get_restaurant_xml, name="get_restaurant_xml"),
         path("get/<int:id>/", get_restaurants_xml_by_id, name="get_restaurants_xml_by_id"),        
