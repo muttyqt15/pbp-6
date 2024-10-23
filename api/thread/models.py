f8abfd316bd868e4d892ebdb27d46c5ea1212816
muttyqt15 <hayaye69@gmail.com>
2024-10-23 14:43:58 +0700

    init threaD

---
+++ api/thread/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from api.authentication.models import User
+
+
+class Thread(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="threads")
+    image = models.ImageField(upload_to="thread/", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    thread = models.ForeignKey(
+        Thread, on_delete=models.CASCADE, related_name="comments"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
