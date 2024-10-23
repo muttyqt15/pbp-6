cee32500c53840e5d00e9b430c998cd494b5c0e7
CrysandyaVic <crysandya.vic@ui.ac.id>
2024-10-23 14:12:19 +0700

    tes

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns: list[URLResolver] = [
     path("auth/", include(("api.authentication.urls", "authentication"))),
     path("restaurant/", include(("api.restaurant.urls", "restaurant"))),
     path("review/", include(("api.review.urls", "review"))),
+
 ]
