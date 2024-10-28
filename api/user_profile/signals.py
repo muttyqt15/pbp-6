c8c0c2fb60985f93aeffedd32f0f4dce1c57233e
Delya04 <adellyaa04@gmail.com>
2024-10-28 21:29:46 +0700

    add

--- api/user_profile/signals.py
+++ api/user_profile/signals.py
@@ -4,10 +4,10 @@ from django.dispatch import receiver
 from .models import OwnerProfile, CustomerProfile
 from api.authentication.models import User
 
-@receiver(post_save, sender=User )
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         if instance.is_resto_owner:
-            OwnerProfile.objects.create(user=instance)
+            OwnerProfile.objects.create(user=instance.resto_owner)
         else:
-            CustomerProfile.objects.create(user=instance)
+            CustomerProfile.objects.create(user=instance.customer)
